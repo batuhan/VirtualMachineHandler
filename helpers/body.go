7817a2f642e0e0524b19c7d28ead6b8bd01cc7ca
sonercirit <sonercirit@gmail.com>
2019-12-03 09:53:34 +0300

    adds body helper

---
+++ helpers/body.go
@@ -0,0 +1,16 @@
+package helpers
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type Body struct {
+	Identifier string
+}
+
+func GetBody(reqBody io.ReadCloser) Body {
+	body := Body{}
+	_ = json.NewDecoder(reqBody).Decode(&body)
+	return body
+}
