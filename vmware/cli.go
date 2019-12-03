f372ff42ae1f577431f618fbb03158f92bc6a25c
sonercirit <sonercirit@gmail.com>
2019-12-03 09:54:00 +0300

    adds new vars and returns error when caught

--- vmware/cli.go
+++ vmware/cli.go
@@ -1,15 +1,22 @@
 package vmware
 
 import (
-	"log"
+	"os"
 	"os/exec"
 )
 
-func execute(arg ...string) string {
+func execute(baseEnv string, arg ...string) ([]byte, error) {
 	cmd := exec.Command("govc", arg...)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with\n%s%s", out, err)
+	cmd.Env = []string{
+		"GOVC_URL=" + os.Getenv(baseEnv+"_GOVC_URL"),
+		"GOVC_USERNAME=" + os.Getenv(baseEnv+"_GOVC_USERNAME"),
+		"GOVC_PASSWORD=" + os.Getenv(baseEnv+"_GOVC_PASSWORD"),
+		"GOVC_DATACENTER=" + os.Getenv(baseEnv+"_GOVC_DATACENTER"),
+		"GOVC_DATASTORE=" + os.Getenv(baseEnv+"_GOVC_DATASTORE"),
+		"GOVC_RESOURCE_POOL=" + os.Getenv(baseEnv+"_GOVC_RESOURCE_POOL"),
+		"GOVC_PASSWORD=" + os.Getenv(baseEnv+"_GOVC_PASSWORD"),
+		"GOVC_RESOURCE_POOL=" + os.Getenv(baseEnv+"_GOVC_RESOURCE_POOL"),
 	}
-	return string(out)
+	out, err := cmd.CombinedOutput()
+	return out, err
 }
