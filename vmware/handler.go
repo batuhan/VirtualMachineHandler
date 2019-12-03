b144223edb9e6c62c307054aa1839a61aff5dc58
sonercirit <sonercirit@gmail.com>
2019-12-03 09:54:57 +0300

    improves error handling on endpoint

--- vmware/handler.go
+++ vmware/handler.go
@@ -2,10 +2,15 @@ package vmware
 
 import (
 	"fmt"
+	"github.com/nodgroup/VirtualMachineHandler/helpers"
 	"net/http"
 )
 
-func Env(w http.ResponseWriter, _ *http.Request) {
-	out := execute("env")
-	_, _ = fmt.Fprintf(w, out)
+func Env(w http.ResponseWriter, req *http.Request) {
+	body := helpers.GetBody(req.Body)
+	out, err := execute(body.Identifier, "env")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	_, _ = fmt.Fprintf(w, string(out))
 }
