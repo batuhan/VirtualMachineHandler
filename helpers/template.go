88c565e895019ea1679274408797f2d7dadb2e60
sonercirit <sonercirit@gmail.com>
2019-12-04 11:00:56 +0300

    adds creation of network templates

--- helpers/template.go
+++ helpers/template.go
@@ -1,10 +1,21 @@
 package helpers
 
-import "github.com/sethvargo/go-password/password"
+import (
+	"encoding/base64"
+	"github.com/sethvargo/go-password/password"
+	"os"
+	"strings"
+)
 
 type User struct {
 	Name              string
 	SshAuthorizedKeys []string `yaml:"ssh-authorized-keys"`
+}
+
+type WriteFile struct {
+	Encoding string
+	Content  string
+	Path     string
 }
 
 type Template struct {
@@ -19,6 +30,23 @@ type Template struct {
 		Mode                   string
 		Devices                []string
 		IgnoreGrowrootDisabled bool `yaml:"ignore_growroot_disabled"`
+	}
+	WriteFiles []WriteFile `yaml:"write_files"`
+	Runcmd     []string
+}
+
+type Network struct {
+	Network struct {
+		Version   int
+		Ethernets struct {
+			Ens192 struct {
+				Addresses   []string
+				Gateway4    string
+				Nameservers struct {
+					Addresses []string
+				}
+			}
+		}
 	}
 }
 
@@ -41,7 +69,7 @@ func GenerateBaseTemplate(sshKey string) *Template {
 	return &template
 }
 
-func AddUbuntuSpecificParameters(template *Template) (*Template, error) {
+func AddUbuntuSpecificParameters(template *Template, networkTemplate []byte) (*Template, error) {
 	newTemplate := template
 
 	newTemplate.Users[0].Name = "ubuntu"
@@ -50,5 +78,19 @@ func AddUbuntuSpecificParameters(template *Template) (*Template, error) {
 		return nil, err
 	}
 	newTemplate.Chpasswd.List = []string{"ubuntu:" + pass}
+
+	newTemplate.WriteFiles = []WriteFile{{Encoding: "base64", Content: base64.StdEncoding.EncodeToString(networkTemplate), Path: "/etc/netplan/50-cloud-init.yaml"}}
+	newTemplate.Runcmd = []string{"netplan apply"}
 	return newTemplate, nil
+}
+
+func CreateNetworkTemplate(identifier string, ipToAssign string) *Network {
+	template := Network{}
+	template.Network.Version = 2
+	ens192 := template.Network.Ethernets.Ens192
+	ens192.Addresses = []string{ipToAssign + "/24"}
+	ens192.Gateway4 = os.Getenv(identifier + "_GATEWAY")
+	ens192.Nameservers.Addresses = strings.Split(os.Getenv(identifier+"_NAMESERVERS"), ",")
+	template.Network.Ethernets.Ens192 = ens192
+	return &template
 }
