eddafc8970f8b3821a9c7f7412c7506fc088cfbe
sonercirit <sonercirit@gmail.com>
2020-04-01 15:20:38 +0300

    archive old vms instead of deleting

--- vmware/actions/delete.go
+++ vmware/actions/delete.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"errors"
+	"fmt"
 	"github.com/google/uuid"
 	"gitlab.com/nod/bigcore/VirtualMachineHandler/helpers"
 	"gitlab.com/nod/bigcore/VirtualMachineHandler/vmware"
@@ -15,13 +17,57 @@ func Delete(identifier string, targetName string, uuid uuid.UUID) error {
 		return err
 	}
 
-	out, err := vmware.Execute(identifier, true, logger, "vm.destroy", vmName)
+	out, err := vmware.Execute(identifier, true, logger, "vm.power", "-off=true", vmName)
+	if err != nil {
+		logger.Println(err.Error())
+		logger.Println(string(out))
+		// disable webhook since vm power off is a pre-condition and a already closed vm will throw an error
+		//go helpers.SendWebhook(helpers.Webhook{
+		//	Uuid:             uuid.String(),
+		//	Step:             "powerOffVM",
+		//	Success:          false,
+		//	ErrorExplanation: err.Error() + "\n" + string(out),
+		//})
+	}
+
+	out, err = vmware.Execute(identifier, true, logger, "object.collect", "-s",
+		fmt.Sprintf("-wait=%s", helpers.Config.PowerOffTimeout),
+		fmt.Sprintf("%s/%s", helpers.Config.DynamicConfigs[identifier].TargetDirectory, vmName),
+		"-runtime.powerState", "poweredOff")
+	if err != nil || len(out) == 0 {
+		var errorExplanation string
+		var powerOffError error
+		if err != nil {
+			logger.Println(err.Error())
+			errorExplanation = err.Error() + "\n" + string(out)
+			powerOffError = err
+		} else {
+			errorExplanation = "can't verify power status after timeout"
+			powerOffError = errors.New(errorExplanation)
+		}
+		logger.Println(string(out))
+		go helpers.SendWebhook(helpers.Webhook{
+			Uuid:             uuid.String(),
+			Step:             "powerOffVM",
+			Success:          false,
+			ErrorExplanation: errorExplanation,
+		}, logger)
+		return powerOffError
+	}
+	go helpers.SendWebhook(helpers.Webhook{
+		Uuid:    uuid.String(),
+		Step:    "powerOffVM",
+		Success: true,
+	}, logger)
+
+	out, err = vmware.Execute(identifier, true, logger, "object.mv", vmName,
+		helpers.Config.DynamicConfigs[identifier].DeleteDirectory)
 	if err != nil {
 		logger.Println(err.Error())
 		logger.Println(string(out))
 		go helpers.SendWebhook(helpers.Webhook{
 			Uuid:             uuid.String(),
-			Step:             "deleteVM",
+			Step:             "archiveVM",
 			Success:          false,
 			ErrorExplanation: err.Error() + "\n" + string(out),
 		}, logger)
@@ -29,7 +75,7 @@ func Delete(identifier string, targetName string, uuid uuid.UUID) error {
 	}
 	go helpers.SendWebhook(helpers.Webhook{
 		Uuid:    uuid.String(),
-		Step:    "deleteVM",
+		Step:    "archiveVM",
 		Success: true,
 	}, logger)
 	return nil
